7dad699227487237355c9e72d65d08228f409c99
XiaZeyu <1215884463@qq.com>
2018-08-14 22:51:41 +0800

    poj

--- base/src/main/java/com/myproject/utils/ZipUtil.java
+++ base/src/main/java/com/myproject/utils/ZipUtil.java
@@ -1,5 +1,7 @@
 package com.myproject.utils;
 
+import java.io.File;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
@@ -24,5 +26,27 @@ public class ZipUtil {
 			return false;
 		}
 	}
+	
+	public static boolean extractAllFiles(File zipFile, String password) {
+		return extractAllFiles(zipFile, BaseUtil.getFilePath(zipFile.getPath()), password);
+	}
+	
+	public static boolean extractAllFiles(File sourceFile, String targetPath, String password) {
+		try {
+			ZipFile zipFile = new ZipFile(sourceFile);
+			zipFile.setFileNameCharset("GBK");
+			if (password != null) {
+				if (zipFile.isEncrypted()) {
+					zipFile.setPassword(password);
+				}
+			}
+			zipFile.extractAll(targetPath);
+			return true;
+		} catch (ZipException e) {
+			return false;
+		}
+	}
+	
+	
 
 }
--- base/src/test/java/com/myproject/TestMain.java
+++ base/src/test/java/com/myproject/TestMain.java
@@ -1,26 +1,34 @@
 package com.myproject;
 
+import java.io.File;
+
+import com.myproject.utils.BaseUtil;
 import com.myproject.utils.PasswordUtil;
 import com.myproject.utils.ZipUtil;
 
 public class TestMain {
 	public static void main(String[] args) {
-		PasswordUtil.initCommonPasswordSet();
-		String pass = "0";
-		int length = 1;
-		long begin = System.currentTimeMillis();
-		while (true) {
-			if (ZipUtil.extractAllFiles("F:\\文档.zip", pass)) {
-				System.out.println("密码测试了" + PasswordUtil.PASSWORD_TEST_TIMES + "次");
-				break;
+		File file = new File("F:\\BaiduNetdiskDownload\\test");
+		File[] listFiles = file.listFiles();
+		for (File tmp : listFiles) {
+			if (ZipUtil.extractAllFiles(tmp,
+					BaseUtil.getFilePath(tmp.getPath()) + "\\" + tmp.getName().substring(0, tmp.getName().length() - 4),
+					"www.fuli10.net")) {
+			} else {
+				System.out.println(tmp.getName());
 			}
-			pass = PasswordUtil.calcNextPassword(pass);
-			if (pass.length() > length) {
-				length = pass.length();
-				System.out.println(length);
-			}
+
 		}
-		long end = System.currentTimeMillis();
-		System.out.println("共花费：" + (end - begin) / 1000 + "秒");
+
+		/*
+		 * PasswordUtil.initCommonPasswordSet(); String pass = "0"; int length = 1; long
+		 * begin = System.currentTimeMillis(); while (true) { if
+		 * (ZipUtil.extractAllFiles("F:\\文档.zip", pass)) { System.out.println("密码测试了" +
+		 * PasswordUtil.PASSWORD_TEST_TIMES + "次"); break; } pass =
+		 * PasswordUtil.calcNextPassword(pass); if (pass.length() > length) { length =
+		 * pass.length(); System.out.println(length); } } long end =
+		 * System.currentTimeMillis(); System.out.println("共花费：" + (end - begin) / 1000
+		 * + "秒");
+		 */
 	}
 }

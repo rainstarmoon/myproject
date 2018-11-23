550262039455960c205f86253c7fd95f619fb25d
XiaZeyu <1215884463@qq.com>
2018-11-23 23:50:56 +0800

    #

--- login/src/main/java/com/myproject/LoginApplication.java
+++ login/src/main/java/com/myproject/LoginApplication.java
@@ -3,14 +3,13 @@ package com.myproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //@EnableTransactionManagement
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 public class LoginApplication {
